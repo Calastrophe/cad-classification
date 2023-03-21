613101aa6490cd127eac6f88b7bde0abe52a7369
Zed Grimm <74928681+Calastrophe@users.noreply.github.com>
2023-03-21 14:20:14 -0500

    first commit

---
+++ src/helpers.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.basicConfig(format="[%(levelname)s]:%(lineno)s - %(message)s", level=logging.DEBUG)
+LOGGER = logging.getLogger()
---
+++ src/parsers.py
@@ -0,0 +1,13 @@
+### This is the file for parsing and extracting nodes from the parsed file
+from helpers import LOGGER
+
+class STPFile:
+    def __init__(self, file: str):
+        pass
+
+
+class STLFile:
+    def __init__(self, file: str):
+        pass
+
+
